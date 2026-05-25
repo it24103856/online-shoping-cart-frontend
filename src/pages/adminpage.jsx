1f8a3db73639b64e6e5659d1e786ac46a3b00601
it24103856 <it24103856@mysliit.lk>
2026-05-26 01:00:11 +0530

    up

--- src/pages/adminpage.jsx
+++ src/pages/adminpage.jsx
@@ -17,7 +17,7 @@ import AdminUserPage from "./admin/adminUserpage";
 import AdminDashboard from "./admin/adminDashboard";
 import AdminOrdersPage from "./admin/adminOrderPage";
 import AdminFeedbackPage from "./admin/adminFeedbackPage";
-import Loader from "../components/Loader";
+import Loader from "../components/Loader.jsx";
 
 export default function AdminPage(){
     const [user, setUser] = useState(null);
